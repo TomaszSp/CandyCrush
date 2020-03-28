25dd80c10519e95a6f993087dd94edb85a52f0da
FranFamen <franciszek.saklawski@gmail.com>
2020-03-28 15:10:29 +0100

    Rendering of the first row

--- core/src/Main_Class/MyGdxGame.java
+++ core/src/Main_Class/MyGdxGame.java
@@ -3,32 +3,30 @@ package Main_Class;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+import Views.StageView;
 
 public class MyGdxGame extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
 
-	
+	StageView blockView;
+
+
 	@Override
 	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("Blocks\\Blocks_01_256x256_Alt_00_002.png");
+
+		blockView = new StageView();
 	}
 
 	@Override
 	public void render () {
 		Gdx.gl.glClearColor((float)0.5, 1, 0, 0);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+		blockView.CreateBlock();
+
 	}
 	
 	@Override
 	public void dispose () {
-		batch.dispose();
-		img.dispose();
+		blockView.DisposeBlock();
 	}
 }
--- core/src/Views/StageView.java
+++ core/src/Views/StageView.java
@@ -1,6 +1,42 @@
 package Views;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
 public class StageView {
  //Tutaj sa przechowane modele 2d ale info o nich jest pobrane z presentera
+    SpriteBatch batch;
+    Texture blockTexture;
+    int NUMBER_OF_COLUMNS = 6;
+    int blockWallLength;
+    int blockXCoordinate;
+    int blockYCoordinate = 0;
+
+    public StageView() {
+    }
+    public void CreateBlock(){
+        batch = new SpriteBatch();
+        blockTexture = new Texture("Blocks\\Blocks_01_256x256_Alt_00_002.png");
+        setBlockWallLength(Gdx.graphics.getWidth() / NUMBER_OF_COLUMNS);
+        batch.begin();
+        for(int i = 0; i < 6; i++){
+            blockXCoordinate = i * getBlockWallLength();
+            batch.draw(blockTexture, blockXCoordinate, blockYCoordinate,  getBlockWallLength(), getBlockWallLength());
+        }
+        batch.end();
+    }
+    public int getBlockWallLength() {
+        return blockWallLength;
+    }
+
+    public void setBlockWallLength(int blockWallLength) {
+        this.blockWallLength = blockWallLength;
+    }
+
+    public void DisposeBlock(){
+        blockTexture.dispose();
+        batch.dispose();
+    }
 
 }
